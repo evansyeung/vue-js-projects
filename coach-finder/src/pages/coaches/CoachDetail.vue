bc5ce859a707c78c3c5f6ce8a5e69e9a05d8b339
Evans Yeung <evansbyeung@gmail.com>
2021-04-01 17:20:06 -0400

    Setup vuex store and fake data

---
+++ coach-finder/src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,6 @@
+<template>
+  DETAILED FOR COACH
+  <router-view></router-view>
+
+  <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
--- coach-finder/src/pages/coaches/CoachesList.vue
+++ coach-finder/src/pages/coaches/CoachesList.vue
@@ -3,6 +3,31 @@
     Filter
   </section>
   <section>
-    List of Coaches
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in coaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
   </section>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    // filteredCoaches() {
+    //   return this.$store.getters["coaches/coaches"];
+    // },
+    // hasCoaches() {
+    //   return this.$store.getters["coaches/hasCoaches"];
+    // },
+    ...mapGetters("coaches", ["coaches", "hasCoaches"]),
+  },
+};
+</script>
