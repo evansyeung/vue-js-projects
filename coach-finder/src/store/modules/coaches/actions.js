e376e9550f4169b8241a75a5e92a8ffcb284b9b2
Evans Yeung <evansbyeung@gmail.com>
2021-04-04 13:47:15 -0400

    Integrate registerCoach with database

--- coach-finder/src/store/modules/coaches/actions.js
+++ coach-finder/src/store/modules/coaches/actions.js
@@ -1,14 +1,30 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
     const { firstName, lastName, description, rate: hourlyRate, areas } = data;
+    const { userId } = context.rootGetters;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName,
       lastName,
       description,
       hourlyRate,
       areas,
     };
-    context.commit("registerCoach", coachData);
+
+    const response = await fetch(
+      `https://vue-http-demo-881d5-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    if (!response.ok) {
+      // error
+    }
+
+    context.commit("registerCoach", {
+      ...coachData,
+      id: userId,
+    });
   },
 };
