801c54c818abdec7a7bc01917cbf30a05c31a3ec
Evans Yeung <evansbyeung@gmail.com>
2021-04-10 13:53:40 -0400

    Implement auto logout

--- coach-finder/src/store/modules/auth/actions.js
+++ coach-finder/src/store/modules/auth/actions.js
@@ -1,7 +1,23 @@
+// Make sure we only have 1 setTimeout function active at a time
+let timer;
+
 export default {
   tryLogin(context) {
     const token = localStorage.getItem("token");
     const userId = localStorage.getItem("userId");
+    const tokenExpiration = localStorage.getItem("tokenExpiration");
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    // If Current time already greater than expiresIn, don't log the user in
+    if (expiresIn < 0) {
+      return;
+    }
+
+    // Set a new auto logout timer with the remaining time left
+    timer = setTimeout(() => {
+      context.dispatch("autoLogout");
+    }, expiresIn);
 
     if (token && userId) {
       context.commit("setUser", {
@@ -27,6 +43,8 @@ export default {
   },
   async auth(context, payload) {
     const { email, password, mode } = payload;
+
+    // TODO: Firebase App was taken down, API key does not work
     let url =
       "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD6_J5XJ530rdhHJnn7sGcSYTblyxqdHBU";
 
@@ -53,20 +71,40 @@ export default {
       throw error;
     }
 
+    // +responseData.expireIn converts it to a number
+    const expiresIn = +responseData.expiresIn * 1000;
+    // getTime() gets current time in milliseconds
+    const expirationDate = new Date().getTime() + expiresIn;
+
     localStorage.setItem("token", responseData.idToken);
     localStorage.setItem("userId", responseData.localId);
+    localStorage.setItem("tokenExpiration", expirationDate);
+
+    // Auto logout
+    timer = setTimeout(() => {
+      context.dispatch("autoLogout");
+    }, expiresIn);
 
     context.commit("setUser", {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
     });
   },
   logout(context) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("tokenExpiration");
+
+    clearTimeout(timer);
+
     context.commit("setUser", {
       token: null,
       userId: null,
-      tokenExpiration: null,
     });
+  },
+  autoLogout(context) {
+    context.dispatch("logout");
+    // If a user was auto logged out we want to redirect them
+    context.commit("setAutoLogout");
   },
 };
