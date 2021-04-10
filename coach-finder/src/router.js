26b03e032b02b688cf46741bf257d29148d9f726
Evans Yeung <evansbyeung@gmail.com>
2021-04-09 21:47:25 -0400

    Add navigation guards

--- coach-finder/src/router.js
+++ coach-finder/src/router.js
@@ -7,6 +7,7 @@ import ContactCoach from "./pages/requests/ContactCoach.vue";
 import RequestsReceived from "./pages/requests/RequestsReceived.vue";
 import UserAuth from "./pages/auth/UserAuth.vue";
 import NotFounded from "./pages/NotFound.vue";
+import store from "./store/index.js";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -34,20 +35,45 @@ const router = createRouter({
     {
       path: "/register",
       component: CoachRegistration,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/requests",
       component: RequestsReceived,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/auth",
       component: UserAuth,
+      meta: {
+        requiresUnauth: true,
+      },
     },
     {
       path: "/:notFounded(.*)",
       component: NotFounded,
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const { isAuthenticated } = store.getters;
+
+  // Pages that require auth
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    // redirect to login/register
+    next("/auth");
+  }
+  // Doesnt make sense to go back to login/register page if already logged in
+  else if (to.meta.requiresUnauth && isAuthenticated) {
+    next("/coaches");
+  } else {
+    next();
+  }
 });
 
 export default router;
